e503feac188ed64706cabdef9b38bcca55044799
geoc1234 <geoc2163@comcast.net>
2022-01-06 06:26:09 -0500

    deleted build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/geo/catkin_ws/src/bones_image
-# Build directory: /home/geo/catkin_ws/src/bones_image/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
